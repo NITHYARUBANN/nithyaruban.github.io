dd11bcdc7ea3f331cc0578526ac0622d81b64e98
NITHYARUBAN <nithyaruban89341@gmail.com>
2025-08-31 22:29:23 +0530

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://nithyaruban.github.io", //For Github Pages
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://nithyaruban.github.io", // important for GitHub Pages
 });
